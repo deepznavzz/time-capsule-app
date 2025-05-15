0f89614a7b3ba830cd1eaa7cd6024d978653648f
Navadeep.Trautz <navadeepsripathi2@gmail.com>
2025-05-15 19:21:41 +0530

    Update API base URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:3001/api' });
+const API = axios.create({ baseURL: 'https://your-backend.onrender.com/api' });
 
 // Add token to requests if available
 API.interceptors.request.use((req) => {
